dc1ffa8dc06d95b05c42484dffff73c4d170120d
Supratim Das <161940938+Supra-tim@users.noreply.github.com>
2026-08-16 18:41:17 +0530

    Time: 3 ms (65.99%), Space: 46.8 MB (28.82%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] kWeakestRows(int[][] mat, int k) {
+        int row=mat.length;
+        int col=mat[0].length;
+        List<int[]>list=new ArrayList<>();
+
+        for(int i=0; i<row; i++){
+            int soldier=0;
+            for(int j=0; j<col; j++){
+                if(mat[i][j]==0){
+                    break;
+                }
+                soldier=soldier+1;
+            }
+            list.add(new int[]{soldier,i});
+        }
+        Collections.sort(list,(e1,e2)->{
+            if(e1[0]==e2[0]){
+                return e1[1]-e1[1];
+            }
+            return e1[0]-e2[0];
+        });
+        int res[]=new int[k];
+        for(int i=0; i<k; i++){
+            res[i]=list.get(i)[1];
+        }
+        return res;
+    }
+}
