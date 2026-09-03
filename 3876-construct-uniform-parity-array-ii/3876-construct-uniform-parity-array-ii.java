06a8e9d8dafa8879c2763daf4488cc90f9a13272
Supratim Das <161940938+Supra-tim@users.noreply.github.com>
2026-09-03 14:09:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int min=Integer.MAX_VALUE;
-        for(int i=0; i<nums1.length; i++){
-            min=Math.min(min, nums1[i]);
-        }
-        if(min%2==1){
-            return true;
-        } else{
-        for(int num: nums1){
-            if(num%2==1){
-                return false;
-            }
-        }
-        }
-        return true;
+        
     }
 }
